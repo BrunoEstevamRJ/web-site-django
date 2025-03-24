531b0932155495c510e639e3d055e6aaa6566f89
BrunoEstevamRJ <bruno.estevam@outlook.com.br>
2025-03-24 18:16:11 -0300

    template

--- website/polls/views.py
+++ website/polls/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
-
 def index(request):
     return HttpResponse("Ola, CHEGUEI CARALHO!")
+
+
+def home(request):
+    return render(request, 'index.html')
 
